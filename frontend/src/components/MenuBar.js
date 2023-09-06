6091725266f1f2357022f633318c4a2af83237ef
wonsun2006 <44808210+wonsun2006@users.noreply.github.com>
2023-09-06 23:34:37 +0900

    feat: 메뉴바 홈 화면 연결
    메뉴바의 로고 클릭 시, 홈 화면으로 이동하도록 연결

--- frontend/src/components/MenuBar.js
+++ frontend/src/components/MenuBar.js
@@ -1,13 +1,16 @@
 import Container from "react-bootstrap/Container";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
+import { Link } from "react-router-dom";
 
 function MenuBar() {
   return (
     <Container>
       <Row className="justify-content-between align-items-center">
         <Col>
-          <h1 className="text-primary">K-COURSE</h1>
+          <Link to="/" style={{ textDecoration: "none" }}>
+            <h1 className="text-primary">K-COURSE</h1>
+          </Link>
         </Col>
         <Col>
           <h6 className="text-end">방원선 교수님</h6>
