c44521e1bddda90d1b9a9de1d60c836b0ed5b824
wonsun2006 <44808210+wonsun2006@users.noreply.github.com>
2023-09-08 21:46:27 +0900

    Update initial_db.sql

--- backend/sql/initial_db.sql
+++ backend/sql/initial_db.sql
@@ -16,9 +16,9 @@ CREATE TABLE IF NOT EXISTS courses (
   course_id INT AUTO_INCREMENT,
   course_name VARCHAR(255),
   edited_time DATETIME,
-  user_id VARCHAR(255),
+  professor_id VARCHAR(255),
   PRIMARY KEY (course_id),
-  FOREIGN KEY (user_id) REFERENCES users(user_id));
+  FOREIGN KEY (professor_id) REFERENCES users(user_id) ON DELETE CASCADE);
   
 CREATE TABLE IF NOT EXISTS posts (
   post_id INT AUTO_INCREMENT,
@@ -27,15 +27,15 @@ CREATE TABLE IF NOT EXISTS posts (
   edited_time DATETIME,
   course_id INT,
   PRIMARY KEY (post_id),
-  FOREIGN KEY (course_id) REFERENCES courses(course_id));
+  FOREIGN KEY (course_id) REFERENCES courses(course_id) ON DELETE CASCADE);
   
 CREATE TABLE IF NOT EXISTS registration (
   registration_id INT AUTO_INCREMENT,
-  user_id VARCHAR(255),
+  student_id VARCHAR(255),
   course_id INT,
   registration_status INT,
   edited_time DATETIME,
   PRIMARY KEY (registration_id),
-  FOREIGN KEY (user_id) REFERENCES users(user_id),
-  FOREIGN KEY (course_id) REFERENCES courses(course_id));
+  FOREIGN KEY (student_id) REFERENCES users(user_id) ON DELETE CASCADE,
+  FOREIGN KEY (course_id) REFERENCES courses(course_id) ON DELETE CASCADE);
   
